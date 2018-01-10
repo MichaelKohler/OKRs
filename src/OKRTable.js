4394bcce09b72231cff10519e5f83b2d4a7b8913
Michael Kohler <me@michaelkohler.info>
2018-01-10 23:59:18 +0100

    Remove Final column

--- src/OKRTable.js
+++ src/OKRTable.js
@@ -31,7 +31,6 @@ class OKRTable extends React.Component {
               <th>Confidence</th>
               <th>Current</th>
               <th>Total</th>
-              <th>Final</th>
             </tr>
           </thead>
           <tbody>
