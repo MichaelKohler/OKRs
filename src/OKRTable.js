b5df6d42999ea33f9f20c89113c0633768cd4ca2
Michael Kohler <me@michaelkohler.info>
2018-01-10 23:40:03 +0100

    Use confidence and tendency instead of risk

--- src/OKRTable.js
+++ src/OKRTable.js
@@ -28,7 +28,7 @@ class OKRTable extends React.Component {
             <tr>
               <th>Description</th>
               <th>Score</th>
-              <th>Risk</th>
+              <th>Confidence</th>
               <th>Current</th>
               <th>Total</th>
               <th>Final</th>
