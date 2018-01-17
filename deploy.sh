4c7c4f2edbce43c4eee7415b09957297b2401645
Michael Kohler <me@michaelkohler.info>
2018-01-17 11:43:18 +0100

    Fix deploy domain

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ chmod 600 travis_deploy_rsa
 ssh-add travis_deploy_rsa
 ssh-keyscan -H me.michaelkohler.info >> ~/.ssh/known_hosts
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/index.html travis@me.michaelkohler:/var/www/html/okrs/
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist travis@me.michaelkohler:/var/www/html/okrs/dist
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/index.html travis@me.michaelkohler.info:/var/www/html/okrs/
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist travis@me.michaelkohler.info:/var/www/html/okrs/dist
