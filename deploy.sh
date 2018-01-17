4e991ccfd324493d61aba237c0bf6200a7ea72d9
Michael Kohler <me@michaelkohler.info>
2018-01-17 11:05:47 +0100

    Move all deploy steps to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,9 @@
+#! /bin/sh
+eval "$(ssh-agent -s)"
+openssl aes-256-cbc -K $encrypted_d1b4660665e6_key -iv $encrypted_d1b4660665e6_iv -in travis_deploy_rsa.enc -out /tmp/travis_deploy_rsa -d
+chmod 600 /tmp/travis_deploy_rsa
+ssh-add /tmp/travis_deploy_rsa
+ssh-keyscan -H me.michaelkohler.info >> ~/.ssh/known_hosts
+
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/index.html travis@me.michaelkohler:/var/www/html/okrs/
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/dist travis@me.michaelkohler:/var/www/html/okrs/dist
