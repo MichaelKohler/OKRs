4b2bdebf1d67aeb1e7fa5ad680f11532fb5847b4
Michael Kohler <me@michaelkohler.info>
2018-01-27 17:39:39 +0100

    Round individual scores to two decimal places

--- src/OKRTableEntry.js
+++ src/OKRTableEntry.js
@@ -4,11 +4,13 @@ import PropTypes from 'prop-types';
 class OKRTableEntry extends React.Component {
   render() {
     const { KeyResult } = this.props;
+    const score = (KeyResult.metrics.current / KeyResult.metrics.total) * 100;
+    const fixedScore = Number.parseFloat(score).toFixed(2);
 
     return (
       <tr>
         <td>{KeyResult.title}</td>
-        <td>{(KeyResult.metrics.current / KeyResult.metrics.total) * 100} %</td>
+        <td>{fixedScore} %</td>
         <td>
           <span className={['risk-indicator', KeyResult.confidence].join(' ')} />
           {KeyResult.tendency}
